be57f42cf0173bcc5d7489c1fe53d0641cfe7d78
Skeltex <dmtr.afanasyev@gmail.com>
2023-10-13 23:28:55 +0400

    My first commit
    My First Commit at this project

---
+++ CalcProject/CalcProject.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "CalcLib.h"
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    string expression = getExpression();
+
+    short expression_size = size(expression);
+
+    string result = solveExpression(expression, expression_size);
+
+
+    printResult(result);
+
+
+    return 0;
+}
